eb7f805403aac35d225194c82461adf7165f787d
Erick Barros <erickbarrosrezende@gmail.com>
2023-04-06 19:17:34 -0300

    Implement render data on table.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,54 @@
+const renderFinancesList = (data) => {
+  const table = document.getElementById("finances-table");
+
+  data.map((item) => {
+    const tableRow = document.createElement("tr");
+    tableRow.className = "mt smaller";
+
+    // title
+    const titleTd = document.createElement("td");
+    const titleText = document.createTextNode(item.title);
+    titleTd.appendChild(titleText);
+    tableRow.appendChild(titleTd);
+
+    // category
+    const categoryTd = document.createElement("td");
+    const categoryText = document.createTextNode(item.name);
+    categoryTd.appendChild(categoryText);
+    tableRow.appendChild(categoryTd);
+
+    // category
+    const dateTd = document.createElement("td");
+    const dateText = document.createTextNode(
+      new Date(item.date).toLocaleDateString()
+    );
+    dateTd.appendChild(dateText);
+    tableRow.appendChild(dateTd);
+
+    // value
+    const valueTd = document.createElement("td");
+    valueTd.className = "center";
+    const valueText = document.createTextNode(
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(item.value)
+    );
+    valueTd.appendChild(valueText);
+    tableRow.appendChild(valueTd);
+
+    // delete
+    const deleteTd = document.createElement("td");
+    deleteTd.className = "right";
+    const deleteText = document.createTextNode("Deletar");
+    deleteTd.appendChild(deleteText);
+    tableRow.appendChild(deleteTd);
+
+    // table add tablerow
+    table.appendChild(tableRow);
+  });
+};
+
 const renderFinanceElements = (data) => {
   const totalItems = data.length;
   const revenues = data
@@ -7,7 +58,6 @@ const renderFinanceElements = (data) => {
     .filter((item) => Number(item.value) < 0)
     .reduce((acc, item) => acc + Number(item.value), 0);
   const totalValue = revenues + expenses;
-
   // render total items
   const financeCard1 = document.getElementById("finance-card-1");
   const totalText = document.createTextNode(totalItems);
@@ -15,7 +65,6 @@ const renderFinanceElements = (data) => {
   totalElement.className = "mt smaller";
   totalElement.appendChild(totalText);
   financeCard1.appendChild(totalElement);
-
   // render revenue
   const financeCard2 = document.getElementById("finance-card-2");
   const revenueText = document.createTextNode(
@@ -28,7 +77,6 @@ const renderFinanceElements = (data) => {
   revenueTextElement.className = "mt smaller";
   revenueTextElement.appendChild(revenueText);
   financeCard2.appendChild(revenueTextElement);
-
   // render expenses
   const financeCard3 = document.getElementById("finance-card-3");
   const expensesText = document.createTextNode(
@@ -41,7 +89,6 @@ const renderFinanceElements = (data) => {
   expensesTextElement.className = "mt smaller";
   expensesTextElement.appendChild(expensesText);
   financeCard3.appendChild(expensesTextElement);
-
   // render balance
   const financeCard4 = document.getElementById("finance-card-4");
   const balanceText = document.createTextNode(
@@ -56,7 +103,6 @@ const renderFinanceElements = (data) => {
   balanceTextElement.appendChild(balanceText);
   financeCard4.appendChild(balanceTextElement);
 };
-
 const onLoadFinancesData = async () => {
   try {
     const date = "2022-12-15";
@@ -72,12 +118,12 @@ const onLoadFinancesData = async () => {
     );
     const data = await result.json();
     renderFinanceElements(data);
+    renderFinancesList(data);
     return data;
   } catch (error) {
     return { error };
   }
 };
-
 const onLoadUserInfo = () => {
   const email = localStorage.getItem("@WalletApp:userEmail");
   const name = localStorage.getItem("@WalletApp:userName");
@@ -99,7 +145,6 @@ const onLoadUserInfo = () => {
   nameElement.appendChild(nameText);
   navbarUserAvatar.appendChild(nameElement);
 };
-
 window.onload = () => {
   onLoadUserInfo();
   onLoadFinancesData();
