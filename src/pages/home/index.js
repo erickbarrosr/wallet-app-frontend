74480d8f5d40317d956724523653572f422bba42
Erick Barros <erickbarrosrezende@gmail.com>
2023-04-10 18:25:46 -0300

    Implement delete.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,41 +1,51 @@
+const onDeleteItem = async (id) => {
+  try {
+    const email = localStorage.getItem("@WalletApp:userEmail");
+
+    await fetch(`https://mp-wallet-app-api.herokuapp.com/finances/${id}`, {
+      method: "DELETE",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        email: email,
+      },
+    });
+    onLoadFinancesData();
+  } catch (error) {
+    alert("Error ao deletar o item.");
+  }
+};
+
 const renderFinancesList = (data) => {
   const table = document.getElementById("finances-table");
   table.innerHTML = "";
-
   const tableHeader = document.createElement("tr");
-
   const titleText = document.createTextNode("Título");
   const titleElement = document.createElement("th");
   titleElement.appendChild(titleText);
   tableHeader.appendChild(titleElement);
-
   const categoryText = document.createTextNode("Categoria");
   const categoryElement = document.createElement("th");
   categoryElement.appendChild(categoryText);
   tableHeader.appendChild(categoryElement);
-
   const dateText = document.createTextNode("Data");
   const dateElement = document.createElement("th");
   dateElement.appendChild(dateText);
   tableHeader.appendChild(dateElement);
-
   const valueText = document.createTextNode("Valor");
   const valueElement = document.createElement("th");
   valueElement.className = "center";
   valueElement.appendChild(valueText);
   tableHeader.appendChild(valueElement);
-
   const actionText = document.createTextNode("Ação");
   const actionElement = document.createElement("th");
   actionElement.className = "right";
   actionElement.appendChild(actionText);
   tableHeader.appendChild(actionElement);
-
   table.appendChild(tableHeader);
-
   data.map((item) => {
     const tableRow = document.createElement("tr");
-
     // title
     const titleTd = document.createElement("td");
     const titleText = document.createTextNode(item.title);
@@ -64,8 +74,11 @@ const renderFinancesList = (data) => {
     );
     valueTd.appendChild(valueText);
     tableRow.appendChild(valueTd);
+
     // delete
     const deleteTd = document.createElement("td");
+    deleteTd.style.cursor = "pointer";
+    deleteTd.onclick = () => onDeleteItem(item.id);
     deleteTd.className = "right";
     const deleteText = document.createTextNode("Deletar");
     deleteTd.appendChild(deleteText);
